eeea8825e216d5162bd58cf54156070c78439a55
Tomasz Brasse <tomasz.brasse@relativity.com>
2021-06-20 20:53:18 +0200

    added font, added spliting segements, when console is short

--- src/Brasse.Powershell.Omnishell/Public/Get-OmnishellPrompt.ps1
+++ src/Brasse.Powershell.Omnishell/Public/Get-OmnishellPrompt.ps1
@@ -3,17 +3,28 @@ function Get-OmnishellPrompt {
         [Parameter(Mandatory)][string] $ConfigFile,
         [string] $ProfileName = "default"
     )
+    $totalSize = 0
+    $consoleSize = $Host.UI.RawUI.WindowSize.Width
     (Get-Content $ConfigFile | ConvertFrom-Json -AsHashtable)."$ProfileName" | ForEach-Object {
         $segmentName = $_.name
         try {
-            foreach ($expression in $_.expressions) {
+            $expressionSize = 0;
+            $expressionsParams = foreach ($expression in $_.expressions) {
                 $prompt = (Invoke-Expression $expression.expression)
-                $params = @{
+                $expressionSize += $prompt.Length
+                @{
                     BackgroundColor = $expression.backgroundColor
                     ForegroundColor = $expression.foregroundColor
                     Prompt          = $prompt
                     NewLine         = [bool]$expression.newline
                 }
+            }
+            if($totalSize + $expressionSize -gt $consoleSize){
+                Write-OmnishellPrompt -NewLine $true
+            } else {
+                $totalSize += $expressionSize
+            }
+            foreach($params in $expressionsParams){
                 Write-OmnishellPrompt @params
             }
         }
