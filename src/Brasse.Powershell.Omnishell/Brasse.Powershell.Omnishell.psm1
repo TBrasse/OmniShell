1a2dae0abba770431eb95c6d765c6058de1c12d5
Tomasz Brasse <tomasz.brasse@gmail.com>
2021-07-11 14:27:38 +0200

    fixed for update-config not being visible

--- src/Brasse.Powershell.Omnishell/Brasse.Powershell.Omnishell.psm1
+++ src/Brasse.Powershell.Omnishell/Brasse.Powershell.Omnishell.psm1
@@ -8,17 +8,17 @@ if($null -eq $Global:Omnishell.Config){
     $Global:Omnishell.Config = "$($Global:Omnishell.AppDir)\config.json"
 }
 
-if(-not (Test-Path -Path $Global:Omnishell.Config)){
-    Copy-Item -Path "$PSScriptRoot\Brasse.Powershell.Omnishell.config.json" -Destination $Global:Omnishell.Config
-} else {
-    Update-Config
-}
-
 $public = Get-ChildItem -Path "$PSScriptRoot\Public\*.ps1"
 $private = Get-ChildItem -Path "$PSScriptRoot\Private\*.ps1"
 
 @($public + $private ) | ForEach-Object {
     Import-Module $_.FullName
+}
+
+if(-not (Test-Path -Path $Global:Omnishell.Config)){
+    Copy-Item -Path "$PSScriptRoot\Brasse.Powershell.Omnishell.config.json" -Destination $Global:Omnishell.Config
+} else {
+    Update-Config
 }
 
 function prompt {
