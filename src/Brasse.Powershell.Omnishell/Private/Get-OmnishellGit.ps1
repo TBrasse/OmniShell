d5f2023437d59fbe86c62ae94ca2b73dd8c659b7
Tomasz Brasse <tomasz.brasse@gmail.com>
2021-09-11 17:14:16 +0200

    added git error stream redirect

--- src/Brasse.Powershell.Omnishell/Private/Get-OmnishellGit.ps1
+++ src/Brasse.Powershell.Omnishell/Private/Get-OmnishellGit.ps1
@@ -1,13 +1,12 @@
 function Get-OmnishellGit {
-
     $fileTypes = @{}
     $result = ""
-    if (git rev-parse --is-inside-work-tree) {
-        $branch = git branch --show-current
+    if (git rev-parse --is-inside-work-tree 2>$null) {
+        $branch = git branch --show-current 2>$null
         $result += $branch
         if([Environment]::OSVersion.Platform -ne "UNIX"){
-            $status = git status -s
-            $commits = git log --oneline origin/$branch..HEAD
+            $status = git status -s 2>$null
+            $commits = git log --oneline origin/$branch..HEAD 2>$null
 
             $status | ForEach-Object {
                 $null = $_ -match "\s*(?<type>\S*) .*"
