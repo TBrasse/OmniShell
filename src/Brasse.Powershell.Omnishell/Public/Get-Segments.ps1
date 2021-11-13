e4d5d4200238482335da7633660a1c785375dd2d
Tomasz Brasse <tomasz.brasse@gmail.com>
2021-11-13 13:01:05 +0100

    removing old files

--- src/Brasse.Powershell.Omnishell/Public/Get-Segments.ps1
+++
@@ -1,8 +0,0 @@
-function Get-Segments {
-    $configFile = $Global:Omnishell.Config
-    $config = Get-Content $configFile | ConvertFrom-Json -AsHashtable
-    $profileName = Invoke-Expression $config.switch
-    $config.profiles[$profileName] | ForEach-Object {
-        $_.name
-    }
-}
