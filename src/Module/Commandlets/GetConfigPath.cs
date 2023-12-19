db05a5004d695e5793bab8010a3f391f53f20e45
TBrasse <tbrasse@gmail.com>
2023-12-19 15:44:06 +0100

    Added GetConfigPath commandlet, for geting config path from console.

---
+++ src/Module/Commandlets/GetConfigPath.cs
@@ -0,0 +1,24 @@
+﻿using Core.Utils;
+using System.Management.Automation;
+
+namespace Module;
+
+[Cmdlet(VerbsCommon.Get, "ConfigPath")]
+[OutputType(typeof(string))]
+public class GetConfigPath : PSCmdlet
+{
+	private static IPathProvider pathProvider;
+
+	public GetConfigPath()
+	{
+		if (pathProvider == null)
+		{
+			pathProvider = OmnishellFactory<ModuleConfiguration>.Build<IPathProvider>();
+		}
+	}
+
+	protected override void ProcessRecord()
+	{
+		WriteObject(pathProvider.GetConfigurationPath());
+	}
+}
